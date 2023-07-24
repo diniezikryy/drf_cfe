fec0f492b46a4b0e660aac87b7591ca73758b720
Dinie Zikry <dzrudi23@gmail.com>
2023-07-25 01:16:07 +0800

    updated create & update methods on serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail', lookup_field='pk')
+    email = serializers.EmailField(write_only=True)
 
     class Meta:
         model = Product
         fields = [
+            'url',
+            'edit_url',
+            'email',
             'pk',
             'title',
             'content',
@@ -16,6 +24,16 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+
+    def create(self, validated_data):
+        obj = super().create(validated_data)
+        return obj
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
